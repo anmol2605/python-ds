ab52fbfd630f5931d9a6cfd51ba0950ec31678f5
Anmol <sharmaanmol269@gmail.com>
2023-07-14 18:34:58 +0530

    loops

---
+++ loop.py
@@ -0,0 +1,16 @@
+from turtle import*
+speed('slowest')
+pencolor("red")
+pensize(3)
+for i in range(6,40,5):
+    fd(80)
+    lt(360/7)
+    dot(10,'green')
+    write(i,font=('Calibri',20,'bold'))
+goto(100,100)
+for i in range(10,0,-1):
+    fd(60)
+    lt(360/10)
+    dot(10,'red')
+    write(i,font=('Calibri',20,'bold'))
+mainloop()
